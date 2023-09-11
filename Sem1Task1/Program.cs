dbeba359b1112c55274d53d208c9b3841a2a62c8
SanSayArt <SanSayArt@gmail.com>
2023-09-11 21:31:57 +0500

    add solution from seminar1 task 0,1,3,5

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()??"0");
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine(firstNumber == Math.Pow(secondNumber,2)? "Да" : "Нет");
---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Console.Write("Введите номер дня недели: ");
+// int day = int.Parse(Console.ReadLine()??"0");
+// string[] dayOfWeek = new string[7];
+// dayOfWeek[0] = "Понедельник";
+// dayOfWeek[1] = "Вторник";
+// dayOfWeek[2] = "Среда";
+// dayOfWeek[3] = "Четверг";
+// dayOfWeek[4] = "Пятница";
+// dayOfWeek[5] = "Суббота";
+// dayOfWeek[6] = "Воскресенье";
+// if (day < 7) Console.WriteLine(dayOfWeek[day-1]);
+// else Console.WriteLine("Неверный номер дня недели.");
+
+
+Console.Write("Введите номер дня недели (выборка из системы): ");
+int day = int.Parse(Console.ReadLine()??"0");
+string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(day));
+
+Console.WriteLine(outDayOfWeek);
