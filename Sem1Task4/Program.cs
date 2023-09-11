1bf6c8533a875f61a8fd1c4b34682611de99fcd3
SanSayArt <SanSayArt@gmail.com>
2023-09-11 21:55:26 +0500

    add solution seminar1 task 4

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()??"0");
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()??"0");
+
+Console.Write("Введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine()??"0");
+
+if (firstNumber > secondNumber && firstNumber > thirdNumber)
+    Console.WriteLine("Первое число максимальное");
+if (secondNumber > firstNumber && secondNumber > thirdNumber)
+    Console.WriteLine("Второе число максимальное");
+if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+    Console.WriteLine("Третье число максимальное");
+
