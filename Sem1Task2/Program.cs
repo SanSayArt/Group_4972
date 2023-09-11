9ecc2cdcdca8fbf45ac29bb361003f2bfde4ec12
SanSayArt <SanSayArt@gmail.com>
2023-09-11 21:48:15 +0500

    Add solution seminar 1 task 2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()??"0");
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()??"0");
+
+if (firstNumber > secondNumber) Console.WriteLine("Первое число больше второго.");
+else if (firstNumber == secondNumber) Console.WriteLine("Введенные числа одинаковые.");
+else Console.WriteLine("Второе число больше первого.");
