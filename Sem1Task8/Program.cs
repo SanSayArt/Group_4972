b3b85a656ad56c8c5a84673dc245ce132abf9d87
SanSayArt <SanSayArt@gmail.com>
2023-09-11 22:06:32 +0500

    change solution 1 task 8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -5,5 +5,8 @@
 Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine()??"0");
 
-for (int i = 1; i < num; i++) Console.Write(i + ", ");
-Console.WriteLine(num);
+for (int i = 1; i < num; i++) 
+{
+    if (i%2 == 0)
+        Console.Write(i + ", ");
+}
