434b7726b95bba327554515da8e193fd49b7e272
SanSayArt <SanSayArt@gmail.com>
2023-09-11 21:41:18 +0500

    add solution from seminar1 task 7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()??"0");
+if (num > 99 && num < 1000)
+{
+    int lastDig = num%10;
+    Console.WriteLine("Последняя цифра числа " + num + " это: "+ lastDig);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
