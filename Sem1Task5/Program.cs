85e0840968f9b962494ec3ff812919eac6f84e09
SanSayArt <SanSayArt@gmail.com>
2023-09-11 21:32:31 +0500

    add solution from seminar1 task 0,1,3,5

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int numN = int.Parse(Console.ReadLine()??"0");
+int enumN = (-1) * numN;
+while (enumN < numN)
+{
+    Console.Write(enumN + ", ");
+    enumN = enumN + 1;
+}
+Console.WriteLine(numN);
