a27809633f798454f6c3b58969fd8fd000a2923d
SanSayArt <SanSayArt@gmail.com>
2023-09-19 22:40:29 +0500

    add solution seminar 4 tasks 24, 26, 28, 30

---
+++ Sem4Task24/Program.cs
@@ -0,0 +1,50 @@
+﻿// -------------------------------------------------------------------------------------------------------------------------------------------------------
+// Задача № 24
+// Напишите программу, которая принимает на вход число (А) и выдает сумму от 1 до А.
+// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Метод для ввода данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Вычисляем сумму чисел
+int Sum(int numb)
+{
+    int sumOfNumber = 0;
+    
+    for (int i = 1; i <= numb; i++)
+    {
+        sumOfNumber += i;
+    }
+    return sumOfNumber;
+}
+
+int GaussSum(int numb)
+{
+    int sumOfNumber = 0;
+    sumOfNumber = numb*(numb+1)/2;
+    return sumOfNumber;
+}
+int numbA = ReadData("Введите число А: ");
+DateTime d1 = DateTime.Now;
+int res1 = Sum(numbA);
+Console.WriteLine(DateTime.Now - d1);
+DateTime d2 = DateTime.Now;
+int res2 = GaussSum(numbA);
+Console.WriteLine(DateTime.Now - d2);
+PrintResult("Сумма чисел от 1 до " + numbA + " (простой) = " + res1);
+PrintResult("Сумма чисел от 1 до " + numbA + " (по Гауссу) = " + res2);
+
+
+
+
+
+
---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Задача №30
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// --------------------------------------------------------------------------------------------------------------------
+
+// Метод для ввода данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+//Заполняем массив генерируемыми числами
+int[] GenArr(int len)
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();
+    for (int i=0; i<len; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+    return arr;
+}
+//Выводим заполненный массив на экран
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i=0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length -1]+"]");
+}
+
+int len = ReadData("Введите длину массива: ");
+PrintArray(GenArr(len));
