8ee99675871588d22f83b7a600ccb88425a0deab
SanSayArt <SanSayArt@gmail.com>
2023-09-20 23:53:18 +0500

    Add solution Seminar 4 tasks 25, 27, 29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,90 @@
+﻿// ------------------------------------------------------------------------------------------------------------------
+// №25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// (задание со *) Написать калькулятор с операциями +, -, /, * и возведение в степень
+// ------------------------------------------------------------------------------------------------------------------
+
+// Метод для считывания введенного числа из консоли
+int ReadNumber(string msg)
+{
+    int num = 0;
+    Console.Write(msg);
+    num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+// Метод для считыванрия знака действия
+string ReadOperand(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
+// Метод сложения
+int Sum(int numA, int numB)
+{
+    int res = 0;
+    res = numA + numB;
+    return res;
+}
+// Метод вычитания
+int Dif(int numA, int numB)
+{
+    int res = 0;
+    res = numA - numB;
+    return res;
+}
+// Метод умножения
+long Mult(int numA, int numB)
+{
+    long res = 0;
+    res = numA * numB;
+    return res;
+}
+// Метод деления
+double Div(int numA, int numB)
+{
+    double res;
+    res = (double) numA / numB;
+    return res;
+}
+// Метод возведения в степень
+long MyPow(int numA, int numB)
+{
+    long res = 1;
+    for(int i=1; i <= numB; i++)
+    {
+        res *= numA;
+    }
+    return res;
+}
+// Метод вывода на экран
+void PrintRes(string msg)
+{
+    Console.WriteLine(msg);
+} 
+
+int numA = ReadNumber("Введите число А: "); 
+int numB = ReadNumber("Введите число B: ");
+string oper = ReadOperand("Введите символ действия (+,-,/,*,^): ");
+// Проверяем введенный символ вычисления и выполняем действие
+switch (oper)
+{
+    case "+":
+        PrintRes($"Сумма чисел {numA} и {numB} равна {Sum(numA, numB)}");
+        break;
+    case "-":
+        PrintRes($"Разность чисел {numA} и {numB} равна {Dif(numA, numB)}");
+        break;
+    case "*":
+        PrintRes($"Произведение чисел {numA} и {numB} равна {Mult(numA, numB)}");
+        break;
+    case "/":
+        PrintRes($"Частное чисел {numA} и {numB} равна {Div(numA, numB)}");
+        break;
+    case "^":    
+        PrintRes($"Возведение числа {numA} в степень {numB} равна {MyPow(numA, numB)}");
+        break;
+    default:
+        PrintRes($"Введеный символ '{oper}' не определен.");
+        break;
+}
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------------------------
+// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// (задание со *) Сделать оценку времени алгоритма через вещественные числа и строки
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// Метод для ввода данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Метод подсчета суммы цифр в числе без перевода в строку 
+int SumDigits(int numb)
+{
+    int res = 0;
+    while(numb > 0)
+    {
+        res += numb%10;
+        numb /= 10;
+    }
+    return res;
+}
+
+int SumDigString(int numb)
+{
+    int res = 0;
+    //string chislo = numb.ToString();
+    char[] digits = numb.ToString().ToCharArray();
+    for (int i = 0; i < digits.Length; i++)
+    {
+        res += int.Parse(digits[i].ToString());
+    }
+    return res;
+}
+int num = ReadData("Введите число: ");
+DateTime time = DateTime.Now; 
+PrintResult($"Сумма цифр в числе: {num} равна {SumDigits(num)}. Вычисления числами были произведены за: {(DateTime.Now - time).ToString()} ");
+time = DateTime.Now; 
+PrintResult($"Сумма цифр в числе: {num} равна {SumDigString(num)}. Вычисления с конвертацией в строку были произведены за: {(DateTime.Now - time).ToString()} ");
