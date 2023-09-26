a7f9f67c75f0368f2346e7d7bbd0acfaf57293d8
SanSayArt <SanSayArt@gmail.com>
2023-09-26 22:36:43 +0500

    change solution 38

--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -6,7 +6,7 @@
 // Для задачи со звездочкой использовать заполнение массива целыми числами.
 // -------------------------------------------------------------------------------------------------------------------------------
 
-
+// Метод заполнения массива вещественными числами
 void FillMass(double[] arr, int lowBorder, int maxBorder)
 {
     Random rnd = new Random();
@@ -15,8 +15,31 @@ void FillMass(double[] arr, int lowBorder, int maxBorder)
         arr[i] = rnd.Next(lowBorder, maxBorder + 1) + Math.Round(rnd.NextDouble(),2);
     }
 }
-// Метод вывода массива на экран
+
+// Метод заполнения массива целыми числами
+void FillMassInt(int[] arr, int lowBorder, int maxBorder)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(lowBorder, maxBorder + 1);
+    }
+}
+
+// Метод составления строки из массива
 string BildStringArr(double[] arr)
+{
+    string res = string.Empty;
+    res = res + "[";
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        res = res + arr[i] + "; ";
+    }
+    res = res + arr[arr.Length - 1] + "]";
+    return res;
+}
+// Метод составления строки из массива
+string BildStringArrInt(int[] arr)
 {
     string res = string.Empty;
     res = res + "[";
@@ -40,7 +63,7 @@ double GetDelta(double[] arr)
         if (min > arr[i]) min = arr[i];
     }
     res = max - min;
-    return res;
+    return Math.Round(res,2);
 }
 // Метод вывода сообщения на экран
 void PrintResult(string msg)
@@ -48,7 +71,69 @@ void PrintResult(string msg)
     Console.WriteLine(msg);
 }
 
+// Сортировка массива вставками
+void InsertSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+
+        // Поиск места для числа в левой части
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
+
+// Обратная сортировка массива подсчетом для произвольного целочисленного диапазона
+void CountingSort(int[] arr)
+{
+    // Поиск минимального и максимального значений в массиве
+    int min = arr[0];
+    int max = arr[0];
+    foreach (int element in arr)
+    {
+        if (element > max) { max = element; }
+        else if (element < min) { min = element; }
+    }
+
+    // Поскольку индекс массива не может быть отрицательным, то приведем минимальное значение диапазона к нулю
+    int correctionFactor = min != 0 ? -min : 0;
+    max += correctionFactor;
+
+    // Создание массива, в котором считаем количество вхождений каждого элемента исходного массива
+    int[] count = new int[max + 1];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        count[arr[i] + correctionFactor]++;
+    }
+
+    // Исходя из данных полученных в предыдущем шаге, формируем отсортированный массив
+    int index = 0;
+    for (int i = count.Length - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < count[i]; j++)
+        {
+            arr[index] = i - correctionFactor;
+            index++;
+        }
+    }
+}
+
 double[] array = new double[8];       // Объявляем массив с определенной длиной
-FillMass(array, 1, 100);        // Заполняем массов
+FillMass(array, 1, 100);        // Заполняем массив
 double delta = GetDelta(array);       // Объявляем переменную для суммы и вычисляем её
 PrintResult(BildStringArr(array) + " -> " + delta);// Вывод результата на экран
+
+int[] arr = new int[8];       // Объявляем массив с определенной длиной
+FillMassInt(arr, 1, 100);        // Заполняем массив целыми числами
+InsertSort(arr);                  // Сортироуем методом вставки
+PrintResult("Массив после сортировки методом вставки: ");
+PrintResult(BildStringArrInt(arr));// Вывод результата на экран
+CountingSort(arr);
+PrintResult("Массив после обратной сортировки методом вставки: ");
+PrintResult(BildStringArrInt(arr));// Вывод результата на экран
